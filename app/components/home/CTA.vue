1d2de8de3d486210be6c263b7e86badaa92c9a73
Kevin Kenfack <142149608+kevinkenfack@users.noreply.github.com>
2024-09-30 05:46:58 +0100

    Update CTA.vue

--- app/components/home/CTA.vue
+++ app/components/home/CTA.vue
@@ -26,7 +26,7 @@
     </div>
     <NuxtLink href="https://drive.google.com/file/d/1DneRpWpaKrHKjAOgfIzzhWKNeOVj8eeP/view?usp=drive_link">
       <img
-        src="https://vercel.com/button"
+        src="https://kevinkenfack-dev.vercel.app/assets/my-cv.svg"
         alt="Download CV"
       >
     </NuxtLink>
